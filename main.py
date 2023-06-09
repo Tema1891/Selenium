e4bf5c95b9efe303ab85c9bf8c518212ae5eef83
Tema1891 <122228074+Tema1891@users.noreply.github.com>
2023-06-09 22:13:43 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+
+
+@pytest.fixture(scope='function', autouse=True)
+def run_mark():
+    # Переходим на страницу авторизации
+    driver.get('http://petfriends.skillfactory.ru/login')
+    # Вводим email
+    driver.find_element(By.ID, 'email').send_keys('frollov.a@mail.ru')
+    # Вводим пароль
+    driver.find_element(By.ID, 'pass').send_keys('Netnett1manstar?')
+    # Нажимаем на кнопку входа в аккаунт
+    driver.find_element(By.CSS_SELECTOR, 'button[type="submit"]').click()
+    driver.find_element(By.CSS_SELECTOR, 'a[href="/my_pets"]').click()
+
+    yield
+
+    driver.quit()
---
+++ one.py
@@ -0,0 +1,38 @@
+
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+def test_search_example():
+    """ Search some phrase in google and make a screenshot of the page. """
+
+    driver = webdriver.Chrome()
+
+    # Open google search page:
+    driver.get('https://google.com')
+
+    time.sleep(3)  # just for demo purposes, do NOT repeat it on real projects!
+
+    # Find the field for search text input:
+    search_input = driver.find_element(By.ID, 'APjFqb')
+
+    # Enter the text for search:
+    search_input.clear()
+    search_input.send_keys('переводчик')
+
+    time.sleep(5)  # just for demo purposes, do NOT repeat it on real projects!
+
+    # Click Search:
+    search_button = driver.find_element(By.NAME, "btnK")
+    search_button.click()
+
+    search_input = driver.find_element(By.ID, 'tw-source-text-ta')
+    search_input.clear()
+    search_input.send_keys('whot?')
+
+
+    time.sleep(10)  # just for demo purposes, do NOT repeat it on real projects!
+
+    # Make the screenshot of browser window:
+    driver.save_screenshot('resul.png')
